9b70efaf6a2c1c95cf66025326482add12521af6
Jo Hyeon Joon <dummy@academind.com>
2024-07-25 05:03:56 +0900

    finished all events

--- loops-in-action.js
+++ loops-in-action.js
@@ -19,3 +19,82 @@ function calculateSum() {
 }   
 
 calculateSumButtonElement.addEventListener('click', calculateSum);
+
+// Highlight links
+
+const highlightLinksButtonElement = document.querySelector('#highlight-links button')
+
+function highlightLinks() {
+    const anchorElements = document.querySelectorAll('#highlight-links a');
+
+    for (const anchorElement of anchorElements) {
+        anchorElement.classList.add('highlight');
+    }
+
+}
+
+highlightLinksButtonElement.addEventListener('click', highlightLinks);
+
+// Display user data
+
+const dummyUserData = {
+    firstName: 'Max',
+    lastName: 'Schwarzmuller',
+    age: 32
+}; 
+
+const displayUserDataButtonElement = document.querySelector('#user-data button');
+
+function displayUserData(){
+    const outputDataElement = document.getElementById('output-user-data');
+
+        outputDataElement.innerHTML = '';
+    for (const key in dummyUserData){
+        const newUserDataListItemElement = document.createElement('li');
+        const outputText = key.toUpperCase() + ': ' + dummyUserData[key];
+        newUserDataListItemElement.textContent = outputText;
+        outputDataElement.append(newUserDataListItemElement);
+    }
+}
+
+displayUserDataButtonElement.addEventListener('click', displayUserData);
+
+// Statistics / Roll the Dice
+
+const rollDiceButtonElement = document.querySelector('#statistics button');
+
+function rollDice() {
+    return Math.floor(Math.random() * 6) + 1; // Math.floor();   5.64 => 5
+}
+
+function deriveNumberofDiceRolls() {
+    const targetNumberInputElement = document.getElementById('user-target-number');
+    const diceRollsListElement = document.getElementById('dice-rolls');
+
+    const enteredNumber = targetNumberInputElement.value;
+    diceRollsListElement.innerHTML ='';
+
+    let hasRolledTargetNumber = false;
+    let numberOfRolls = 0;
+
+    while (!hasRolledTargetNumber){
+        const rolledNumber = rollDice();
+        // if (rolledNumber == enteredNumber){
+        //     hasRolledTargetNumber = true;
+        // }
+        numberOfRolls++;
+        const newRollListItemElement = document.createElement('li');
+        const outputText = 'Roll' + numberOfRolls + ': ' + rolledNumber;
+        newRollListItemElement.textContent = outputText;
+        diceRollsListElement.append(newRollListItemElement);
+        hasRolledTargetNumber = rolledNumber == enteredNumber;
+    }
+
+    const outputTotalRollsElement = document.getElementById('output-total-rolls');
+    const outputTargetNumberElement = document.getElementById('output-target-number');
+
+    outputTargetNumberElement.textContent = enteredNumber;
+    outputTotalRollsElement.textContent = numberOfRolls;
+}
+
+    rollDiceButtonElement.addEventListener('click', deriveNumberofDiceRolls);
